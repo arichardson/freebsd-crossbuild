26cc993e501fe3c84e32c6487d31697a7496d2f1
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2017-09-09 15:40:42 +0100

    Fix build of usr.bin/find on Mac
    MacOS doesn't have rpmatch()

--- include/mac/pre-include.h
+++ include/mac/pre-include.h
@@ -8,7 +8,8 @@
 #define st_atim st_atimespec
 #define st_mtim st_mtimespec
 #define st_ctim st_ctimespec
-#define st_btim st_btimespec
+#define st_btim st_birthtimespec
+#define st_birthtim st_birthtimespec
 #define st_atimensec st_atimespec.tv_nsec
 #define st_mtimensec st_mtimespec.tv_nsec
 #define st_ctimensec st_ctimespec.tv_nsec
---
+++ include/mac/stdlib.h
@@ -0,0 +1,4 @@
+#pragma once
+#include_next <stdlib.h>
+
+int rpmatch(const char *response);
