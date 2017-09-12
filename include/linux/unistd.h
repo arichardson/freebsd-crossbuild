c72794318854790f74ddf7ea919f3f4ec6b417be
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2017-09-12 14:42:28 +0100

    Make sure that getopt is POSIX compatible
    This makes sure that find doesn't continue parsing arguments past the path

---
+++ include/linux/unistd.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include_next <unistd.h>
+#undef getopt
+
+// Force POSIX compatibility in glibc getopt
+static inline int getopt_real(int argc, char * const argv[], const char *optstring)
+{
+#ifdef __GLIBC__
+  // If the first character of optstring is '+' or the environment variable
+  // POSIXLY_CORRECT is set, then option processing stops as soon as a
+  // nonoption argument is encountered.
+  char* newoptstring = NULL;
+  asprintf(&newoptstring, "+%s", optstring);
+  int ret = bsd_getopt(argc, argv, newoptstring);
+  free(newoptstring);
+  return ret;
+#else
+  return bsd_getopt(argc, argv, optstring)
+#endif
+}
+
+#define getopt getopt_real
